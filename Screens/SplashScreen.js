eaabf34221f06bfe12a8afe1fd49779e1d75b468
Amine <amine.swagg99@gmail.com>
2025-01-09 01:17:41 +0100

    Initial commit

---
+++ Screens/SplashScreen.js
@@ -0,0 +1,59 @@
+import { View, Text, StyleSheet, StatusBar, Image } from "react-native";
+import React, { useEffect } from "react";
+import { AppColors } from "./../Utilities/AppColors";
+import { useNavigation } from "@react-navigation/native";
+const AppLogo = require("./../assets/adaptive-icon.png");
+
+const SplashScreen = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace("Signup");
+    }, 3000);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor={AppColors.primary} />
+      <View style={styles.contentContainer}>
+        <Image style={styles.logoImage} source={AppLogo} />
+        <View>
+          <Text style={styles.logoText}>ShopApp </Text>
+          <Text style={styles.logoUnderText}>Happy Shopping! </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: AppColors.primary,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  contentContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 15,
+  },
+  logoImage: {
+    width: 80,
+    height: 80,
+  },
+  logoText: {
+    fontSize: 40,
+    color: AppColors.secondary,
+  },
+  logoUnderText: {
+    color: AppColors.secondary,
+    letterSpacing: 2,
+    textAlign: "center",
+    top: -4,
+  },
+});
---
+++ Utilities/AppColors.js
@@ -0,0 +1,5 @@
+export const AppColors = {
+  primary: "#E74C3C",
+  secondary: "#ECF0F1",
+  accent: "#000000",
+};
